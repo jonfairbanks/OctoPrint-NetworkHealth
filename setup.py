d962d9bb8395c24a30e0bb5fe55a684c8c126b58
Jon Fairbanks <fairplay89@gmail.com>
2021-04-25 12:30:44 -0700

    Dependency Fixes

--- setup.py
+++ setup.py
@@ -33,10 +33,7 @@ plugin_url = "https://github.com/jonfairbanks/OctoPrint-Wifihealth"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = [
-	"os",
-	"time"
-]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
