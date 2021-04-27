bce596c74cb2e11bcf314ccaa15e94a216644863
Jon Fairbanks <fairplay89@gmail.com>
2021-04-26 20:27:06 -0700

    Suggested Changes

--- octoprint_NetworkHealth/__init__.py
+++ octoprint_NetworkHealth/__init__.py
@@ -1,21 +1,47 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
+import os
+import time
 
-import octoprint.plugin
+from octoprint.util import RepeatedTimer
 
 class NetworkHealthPlugin(octoprint.plugin.StartupPlugin):
+    def __init__(self):
+        self._timer = None
+
     def on_after_startup(self):
         self._logger.info("Beginning to monitor Network health...")
+        self._timer = RepeatedTimer(180, self._check_network)
+        self._timer.start()
 
+    def _check_network(self):
+        try:
+              if not check_ping():
+                  self._logger.error("No Network Connection - Resetting Adapter(s)...")
+                  reset_wlan0 = 'ip link set wlan0 down; ip link set wlan0 up'
+                  reset_eth0  = 'ip link set eth0 down; ip link set eth0 up'
+                  os.system(reset_wlan0)
+                  os.system(reset_eth0)
+        except Exception:
+            self._logger.exception("Could not run network health check")
 
-__plugin_name__ = "Network Health Monitor"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "Monitors the health of the Network connection and restarts it if necessary"
-__plugin_pythoncompat__ = ">=2.7,<4"
-__plugin_implementation__ = NetworkHealthPlugin()
+    def get_update_information(self, *args, **kwargs):
+        return dict(
+            networkhealth=dict(
+                displayName=self._plugin_name,
+                displayVersion=self._plugin_version,
+
+                type="github_release",
+                current=self._plugin_version,
+                user="jonfairbanks",
+                repo="OctoPrint-NetworkHealth",
+
+                pip="https://github.com/jonfairbanks/OctoPrint-NetworkHealth/archive/{target}.zip"
+            )
+        )
+
 
 def default_gateway():
-    import socket, struct
+    import socket
+    import struct
     with open("/proc/net/route") as fh:
         for line in fh:
             fields = line.strip().split()
@@ -23,45 +49,35 @@ def default_gateway():
                 continue
             return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
-def __init__():
-    import os, time
-    while True:
-        def check_ping():
-            hostname = default_gateway()
-            response = os.system("ping -c 4 " + hostname)
-            if response == 0:
-                pingstatus = 0
-            else:
-                pingstatus = 1
+def check_ping():
+    hostname = default_gateway()
+    response = os.system("ping -c 4 " + hostname)
+    if response == 0:
+        return True
+    else:
+        return False
 
-            return pingstatus
 
-        networkstatus = check_ping()
+__plugin_name__ = "Network Health Monitor"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Monitors the health of the Network connection and restarts it if necessary"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
-        if networkstatus == 1:
-            self._logger.error("No Network Connection - Resetting Adapter(s)...")
-            reset_wlan0 = 'ip link set wlan0 down; ip link set wlan0 up'
-            reset_eth0  = 'ip link set eth0 down; ip link set eth0 up'
-            os.system(reset_wlan0)
-            os.system(reset_eth0)
-            time.sleep(180)
-            continue
+def __plugin_check__():
+    import sys
 
-def get_update_information(*args, **kwargs):
-    return dict(
-        updateplugindemo=dict(
-            displayName=self._plugin_name,
-            displayVersion=self._plugin_version,
+    if not sys.platform.startswith("linux"):
+        import logging
+        logging.getLogger("octoprint.plugins.networkhealth").error("Cannot run this plugin under anything but Linux")
+        return False
 
-            type="github_release",
-            current=self._plugin_version,
-            user="jonfairbanks",
-            repo="OctoPrint-NetworkHealth",
+    return True
 
-            pip="https://github.com/jonfairbanks/OctoPrint-NetworkHealth/archive/{target}.zip"
-        )
-    )
+def __plugin_load__():
+    global __plugin_implementation__
+    global __plugin_hooks__
 
-__plugin_hooks__ = {
-"octoprint.plugin.softwareupdate.check_config": get_update_information
-}
+    __plugin_implementation__ = NetworkHealthPlugin()
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
